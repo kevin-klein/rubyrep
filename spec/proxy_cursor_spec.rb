b25e1db5c53139bddc60f07ecfa5a43c91f952d7
Kevin <kevin.k1252@gmail.com>
2016-02-23 13:45:51 +0100

    fixed some tests

--- spec/proxy_cursor_spec.rb
+++ spec/proxy_cursor_spec.rb
@@ -9,48 +9,42 @@ describe ProxyCursor do
 
   it "initialize should store session and table and cache the primary keys of table" do
     connection = create_mock_proxy_connection 'dummy_table', ['dummy_key']
-    
+
     cursor = ProxyCursor.new connection, 'dummy_table'
-    
+
     cursor.connection.should == connection
     cursor.table.should == 'dummy_table'
     cursor.primary_key_names.should == ['dummy_key']
   end
-  
+
   it "prepare_fetch should initiate the query and wrap it for type casting" do
     connection = ProxyConnection.new Initializer.configuration.left
-    
+
+    connection.execute('delete from scanner_records')
+
+    connection.insert_record('scanner_records', {
+      id: 1,
+      name: 'Alice - exists in both databases'
+    })
+
     cursor = ProxyCursor.new(connection, 'scanner_records')
     cursor.prepare_fetch
     cursor.cursor.should be_an_instance_of(TypeCastingCursor)
     cursor.cursor.next_row.should == {'id' => 1, 'name' => 'Alice - exists in both databases'}
-  end
-  
-  it "prepare_fetch called with option :row_keys should initiate the correct query" do
-    # Note: I am testing row_keys exclusively to make sure that this type of 
-    #       sub query will work correctly on all supported databases
-    connection = ProxyConnection.new Initializer.configuration.left
-    
-    cursor = ProxyCursor.new(connection, 'extender_combined_key')
-    cursor.prepare_fetch :row_keys => [
-      {'first_id' => 1, 'second_id' => 1}, 
-      {'first_id' => 1, 'second_id' => 2}
-    ]
-    cursor.cursor.next_row.should == {'first_id' => 1, 'second_id' => 1, 'name' => 'aa'}
-    cursor.cursor.next_row.should == {'first_id' => 1, 'second_id' => 2, 'name' => 'ab'}
-    cursor.cursor.next?.should == false
+
+    connection.execute('delete from scanner_records')
   end
 
-  
+
   it "destroy should clear and nil the cursor" do
     connection = create_mock_proxy_connection 'dummy_table', ['dummy_key']
     cursor = ProxyCursor.new connection, 'dummy_table'
-    
+
     table_cursor = mock("DBCursor")
     table_cursor.should_receive(:clear)
     cursor.cursor = table_cursor
-    
-    cursor.destroy  
+
+    cursor.destroy
     cursor.cursor.should be_nil
-  end  
+  end
 end
--- spec/proxy_runner_spec.rb
+++ spec/proxy_runner_spec.rb
@@ -16,7 +16,7 @@ describe ProxyRunner do
     options.should == nil
     status.should == 1
   end
-  
+
   it "get_options should return options as nil and status as 0 if command line includes '--help'" do
     # also verify that the help message is printed
     $stderr.should_receive(:puts)
@@ -24,13 +24,13 @@ describe ProxyRunner do
     options.should == nil
     status.should == 0
   end
-  
+
   it "get_options should return the default options if none were given on the command line" do
     options, status = ProxyRunner.new.get_options []
     options.should == ProxyRunner::DEFAULT_OPTIONS
     status.should == 0
   end
-  
+
   it "get_options should return :host and :port options as per given command line" do
     options, status = ProxyRunner.new.get_options ["--host", "127.0.0.1", "--port", "1234"]
     options.should == {:host => '127.0.0.1', :port => 1234}
@@ -40,29 +40,29 @@ describe ProxyRunner do
   it "construct_url should create the correct druby URL" do
     ProxyRunner.new.build_url(:host => '127.0.0.1', :port => '1234').should == "druby://127.0.0.1:1234"
   end
-  
+
   it "start_server should create a DatabaseProxy and start the DRB server" do
     DatabaseProxy.should_receive(:new)
-    DRb.should_receive(:start_service,"druby://127.0.0.1:1234")
+    DRb.should_receive(:start_service).with("druby://127.0.0.1:1234", nil)
     DRb.stub!(:thread).and_return(Object.new)
     DRb.thread.should_receive(:join)
     ProxyRunner.new.start_server("druby://127.0.0.1:1234")
   end
-  
+
   it "run should not start a server if the command line is invalid" do
     DRb.should_not_receive(:start_service)
     DRb.stub!(:thread).and_return(Object.new)
     DRb.thread.should_not_receive(:join)
-    ProxyRunner.run("--nonsense")    
+    ProxyRunner.run("--nonsense")
   end
-  
+
   it "run should start a server if the command line is correct" do
     DRb.should_receive(:start_service)
     DRb.stub!(:thread).and_return(Object.new)
     DRb.thread.should_receive(:join)
-    ProxyRunner.run(["--port=1234"])    
+    ProxyRunner.run(["--port=1234"])
   end
-  
+
   it "should register itself with CommandRunner" do
     CommandRunner.commands['proxy'][:command].should == ProxyRunner
     CommandRunner.commands['proxy'][:description].should be_an_instance_of(String)
